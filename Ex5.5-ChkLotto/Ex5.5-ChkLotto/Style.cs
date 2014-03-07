cb76ae74f5ad721795b09a4cfaa445f584774ac3
Ferenc Grecu <mivor20@gmail.com>
2014-03-07 12:41:12 +0200

    Change red color

---
+++ Ex5.5-ChkLotto/Ex5.5-ChkLotto/Style.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Ex5._5_ChkLotto
+{
+    public struct Style
+    {
+        public static Color background = Color.BlanchedAlmond;
+        public static Color border = Color.Gray;
+        public static Color hover = Color.White;
+        public static Color active = Color.Orange;
+        public static Color win = Color.YellowGreen;
+        public static Color lose = Color.IndianRed;
+    }
+}
