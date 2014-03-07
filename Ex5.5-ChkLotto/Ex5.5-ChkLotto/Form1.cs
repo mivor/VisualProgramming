3d43a70b8ba091e2608bedf522da9cbe66a38bdd
Ferenc Grecu <mivor20@gmail.com>
2014-03-07 10:32:21 +0200

    Working UI prototype

--- Ex5.5-ChkLotto/Ex5.5-ChkLotto/Form1.cs
+++ Ex5.5-ChkLotto/Ex5.5-ChkLotto/Form1.cs
@@ -14,43 +14,76 @@ namespace Ex5._5_ChkLotto
     {
         public Form1()
         {
-            InitializeComponent();
+            //InitializeComponent();
             InitUI();
         }
 
         private void InitUI()
         {
             int n = 1;
+            int startX = 20;
             int paddingX = 40;
             int paddingY = 40;
-            int posX = 30;
-            int posY = 30;
+            int posX = startX;
+            int posY = 20;
 
             this.AutoSize = true;
+            this.Text = "LOTTO";
 
             do
             {
                 CheckBox chkLotto = new CheckBox();
                 chkLotto.Name = "chkLotto" + n;
                 chkLotto.Location = new Point(posX, posY);
-                chkLotto.Width = 40;
+                chkLotto.Size = new Size(30, 30);
+                chkLotto.Margin = new Padding(0,0,startX,paddingY);
 
                 chkLotto.Appearance = Appearance.Button;
+                chkLotto.BackColor = Color.BlanchedAlmond;
+                chkLotto.FlatStyle = FlatStyle.Flat;
+                chkLotto.FlatAppearance.CheckedBackColor = Color.Yellow;
+                chkLotto.FlatAppearance.MouseOverBackColor = Color.White;
+                chkLotto.FlatAppearance.MouseDownBackColor = Color.White;
+                chkLotto.FlatAppearance.BorderColor = Color.Black;
+
                 chkLotto.Text = n.ToString();
                 chkLotto.TextAlign = ContentAlignment.MiddleCenter;
+
+                chkLotto.Click += chkLotto_Click;
+
+                Controls.Add(chkLotto);
 
                 if (n % 10 == 0)
                 {
                     posY += paddingY;
-                    posX = 0;
+                    posX = startX;
                 }
-
-                Controls.Add(chkLotto);
-
-                posX += paddingX;
+                else posX += paddingX;
+                
                 ++n;
 
             } while (n < 50);
+
+            posY += paddingY * 2;
+
+            Button btnStart = new Button();
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.Location = new Point(startX, posY);
+            btnStart.Size = new Size(70, 40);
+            btnStart.Margin = new Padding(0, 0, 0, paddingY);
+
+            btnStart.Text = "Win!";
+            btnStart.TextAlign = ContentAlignment.MiddleCenter;
+
+            Controls.Add(btnStart);
+
+        }
+
+        private void chkLotto_Click(object sender, EventArgs e)
+        {
+            CheckBox origin = sender as CheckBox;
+
+            origin.Text = "C!";
         }
     }
 }
